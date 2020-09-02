46fe1a9277ebcc02e3d0d1f2dfe7289b9f7647c2
Reina <reinaxxxxa@gmail.com>
2020-09-02 23:18:10 +0800

    从数据库中加载渲染
    主页可以从数据库加载商品数据并渲染了
    删除冗杂文件

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var path = require('path');
 const database = require('../database');
 const { join } = require('path');
+// const { send } = require('process');
+const ejs = require('ejs');
+// const { render } = require('pug');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{
-    itemPicPath: 'images/ibasso-dx160.jpg',
-    itemName: 'iBasso dx160 播放器',
-    itemPrice: 1800,
-    itemDesc: '作为一款定位于不足3K的中端HIFI播放器，iBasso DX160相比上一代DX150来说，虽然取消了可更换耳放卡设计，对于资深老烧来说，缺少了后期更多可玩性和素质提升，但对于中意这个价格区间的hifi初烧友来说，DX160具有更时尚的高颜值外观，以及相比DX150标配版更好听的声音潜力，无疑是预算有限的烧友一个理想的选择。在笔者这几天的试听过程中，DX160还算是一个杂食性不错的播放器，从参数上来看，推力和适配上无论是对于不同耳塞的驱动性和适配性，还是对于流行、古典、摇滚等不同风格的包容性，DX160都表现出了出色的宽容度，兼具耐听、好听的特点。',
-    trans:{
-      postSupport: true,
-      faceToFace: true,
-      noNeedTrans: true
-    },
-    oldNewRate: 95
-  });
+  let query_sql = 'SELECT * FROM item';
+  database.connection.query(query_sql, function(err, result){
+    if (err) console.error(err);
+    let cardSet = ''
+    result.forEach(item => {
+      cardDir = join(__dirname, '../views/especial/index/card.ejs');
+      generator = ejs.compile(fs.readFileSync(cardDir, 'utf8'));
+      cardSet += generator({
+        itemPicPath : 'images/Reina.jpg',
+        itemName : item.item_name,
+        itemPrice : item.item_price,
+        itemDesc: item.item_desc,
+        trans:{
+          postSupport: item.trans_post? true : false,
+          faceToFace:  item.trans_face? true : false,
+          noNeedTrans: item.trans_null? true : false
+        },
+        oldNewRate: item.old_new_rate
+      }); 
+    });
+    res.render('index', {
+      cardSet: cardSet
+    });
+  });  
 });
 
 module.exports = router;
--- myapp/routes/submit.js
+++ myapp/routes/submit.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 const database = require('../database');
 
 /* GET home page. */
@@ -7,7 +7,7 @@ router.use(function(req, res, next) {
     itemName = req.body.itemName;
     price = req.body.itemDesc;
 
-    var insert_sql = 'INSERT INTO test (item_name, price) VALUES (' + '\'' +  itemName + '\'' + ',' + price + ')';
+    let insert_sql = 'INSERT INTO test (item_name, price) VALUES (' + '\'' +  itemName + '\'' + ',' + price + ')';
     console.log(insert_sql);
     database.connection.query(insert_sql);
     //res.send('提交完成，三秒后重定向至主页...');
@@ -18,8 +18,3 @@ router.use(function(req, res, next) {
 });
 
 module.exports = router;
-
-
-// `INSERT INTO item_info(item_name, owner_id, submission_date, '
-//                     + 'item_price, item_description, old_new_rate, transportation, remark)`
-//                     + 'VALUES(${req.body})'
