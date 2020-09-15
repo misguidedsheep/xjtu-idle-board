03ef681186dd940444959a41f31feba31f68d481
Reina <reinaxxxxa@gmail.com>
2020-09-15 20:55:12 +0800

    完善发布页, 适配新数据表, 预定主页特性添加
    -  为发布页添加表单验证(前端)
    -  主页添加滚动监听, 浏览商品时itemNav取代topNav置顶, 后续将排序搜索改为ajax方式, 将实现瀑布流商品展示, 动态请求加载
    -  优化卡片显示, 后续将提升卡片展示体验: 使用模态框(Modal)展示更多信息
    -  删除多余注释
    -  细节优化与适配

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -12,20 +12,19 @@ function sql_respond(sql, res){
   database.connection.query(sql, function(err, result){
     if (err) console.error(err);
     let cardSet = ''
+    var cardDir = join(__dirname, '../views/especial/index/card.ejs');
+    var generator = ejs.compile(fs.readFileSync(cardDir, 'utf8'));
     result.forEach(item => {
-      cardDir = join(__dirname, '../views/especial/index/card.ejs');
-      generator = ejs.compile(fs.readFileSync(cardDir, 'utf8'));
       cardSet += generator({
-        itemPicPath : 'images/Reina.jpg',
-        itemName : item.item_name,
-        itemPrice : item.item_price,
-        itemDesc: item.item_desc,
+        coverFileName : item.CoverFileName,
+        itemName : item.ItemName,
+        itemPrice : item.ItemPrice,
+        itemDescription: item.ItemDescription,
         trans:{
-          postSupport: item.trans_post? true : false,
-          faceToFace:  item.trans_face? true : false,
-          noNeedTrans: item.trans_null? true : false
-        },
-        oldNewRate: item.old_new_rate
+          deliverByPost: item.DeliverByPost? true : false,
+          deliverByFace:  item.DeliverByFace? true : false,
+          deliverNoNeed: item.DeliverNoNeed? true : false
+        }
       }); 
     });
     res.render('index', {
@@ -43,23 +42,22 @@ router.get('/search=:searchExp-sort=:sortMode-min=:minPrice-max=:maxPrice', func
   let sortMode = req.params.sortMode;
   let searchExp = req.params.searchExp;
   
-  let rangeCode = ' item_price BETWEEN ' + req.params.minPrice + ' and ' + req.params.maxPrice;
+  let rangeCode = ' ItemPrice BETWEEN ' + req.params.minPrice + ' and ' + req.params.maxPrice;
   
   // for-debug
   console.log('SORT MATCHED!')
   
   switch (sortMode){
-    case 'price_up': sortCode = ' ORDER BY item_price ASC'; break;
-    case 'price_down': sortCode = ' ORDER BY item_price DESC'; break;
-    case 'date_down': sortCode = ' ORDER BY item_id DESC'; break;
-    case 'new_to_old': sortCode = ' ORDER BY old_new_rate DESC'; break
+    case 'price_up': sortCode = ' ORDER BY ItemPrice ASC'; break;
+    case 'price_down': sortCode = ' ORDER BY ItemPrice DESC'; break;
+    case 'date_down': sortCode = ' ORDER BY ItemID DESC'; break;
     case 'null': toSort = false; break;
     default: res.send('Invalid Sort Method ERROR'); break;
   }
 
   if(searchExp == 'null' ||  searchExp == '') toSearch = false; 
-  else searchCode = " and item_name LIKE \'%" + searchExp + "%\'";
-  let sqlCode = 'SELECT * FROM item WHERE' + rangeCode + (toSearch? searchCode : '') + (toSort? sortCode : ''); 
+  else searchCode = " and ItemName LIKE \'%" + searchExp + "%\'";
+  let sqlCode = 'SELECT * FROM Items WHERE' + rangeCode + (toSearch? searchCode : '') + (toSort? sortCode : ''); 
   sql_respond(sqlCode, res)
 });
 
@@ -67,7 +65,7 @@ router.get('/search=:searchExp-sort=:sortMode-min=:minPrice-max=:maxPrice', func
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let query_sql = 'SELECT * FROM item ORDER BY item_id desc';
+  let query_sql = 'SELECT * FROM Items ORDER BY ItemID desc';
   sql_respond(query_sql, res);
 });
 
--- myapp/routes/tosubmit.js
+++ myapp/routes/tosubmit.js
@@ -6,26 +6,22 @@ const constant = require('../private/constant');
 
 /* GET home page. */
 router.use(function(req, res, next) {
-    // itemName = req.body.itemName;
-    // price = req.body.itemDesc;
-
-    // let insert_sql = 'INSERT INTO test (item_name, price) VALUES (' + '\'' +  itemName + '\'' + ',' + price + ')';
-    // console.log(insert_sql);
-    // database.connection.query(insert_sql);
-    // res.send('提交完成，三秒后重定向至主页...');
-    // setTimeout(() => {
-    //     res.redirect('/')
-    // }, 3000);
-    let getToken = req.headers.authorization.replace("Bearer ", "");
-    jwt.verify(getToken, constant.secretKey, function(err, decoded){
-        if(err){
-            res.log(err);
-            res.end(`ERROR: ${err.message}`);
-        }else{
-            res.send('Submit Success! (from Router: toSubmit.js)');
+    form = JSON.parse(req.body.formData)
+    sql = 'Insert into Items' + 
+            ' (ItemName, ItemPrice, ItemDescription, ItemOldNewRate, ' + 
+            'CoverFileName, DeliverByPost,' + 
+            ' DeliverByFace, DeliverNoNeed, ' + 
+            'Remarks, UserName) values' + `('${form.itemName}', ${form.itemPrice}, '${form.itemDescription}', '${form.itemOldNewRate}', ` + 
+            `'${form.coverFileName}', ${(form.hasOwnProperty("deliverByPost"))? true: false}, ` + 
+            `${(form.hasOwnProperty("DeliverByFace"))? true: false}, ${(form.hasOwnProperty("deliverNoNeed"))? true: false}, ` + 
+            `'${form.remarks}' , '${form.userName}')`;
+    result = database.connection.query(sql, function(err, result){
+        if(err) console.error(err);
+        else{
+            console.log(result);
         }
     })
-    
+    res.send("Database Query OK")
 });
 
 module.exports = router;
