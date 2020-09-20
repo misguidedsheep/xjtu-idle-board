b77ea876b75671b7a52d2c4199da598562656d7b
Reina <reinaxxxxa@gmail.com>
2020-09-20 10:29:04 +0800

    调用TinyPNG API，实现上传图片压缩

--- myapp/routes/saveimg.js
+++ myapp/routes/saveimg.js
@@ -1,11 +1,18 @@
 let express = require('express');
 let router = express.Router();
+var tinify = require("tinify");
+var constant = require("../private/constant")
+var path = require("path")
 
-// 
+tinify.key = constant.tinifyKey;
+
 router.use(function(req, res, next){
-    // 返回文件名
+    // 调用TinyPNG API，实现压缩
+    var picFileName = req.file.filename;
+    var picFilePath = path.join( __dirname, `../public/upload/${picFileName}`)
+    tinify.fromFile(picFilePath).toFile(picFilePath);
     res.json({
-        fileName: req.file.filename
+        fileName: picFileName
     })
 })
 
